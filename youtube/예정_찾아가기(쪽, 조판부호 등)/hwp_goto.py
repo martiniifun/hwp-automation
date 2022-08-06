c3ca83408cdbe8a6dfdbb5ee3665ef46dcd437b6
신명진 <martinii.fun@gmail.com>
2022-08-07 02:05:54 +0900

    특정페이지로 찾아가는 함수 만들기

--- youtube/예정_찾아가기(쪽, 조판부호 등)/hwp_goto.py
+++ youtube/예정_찾아가기(쪽, 조판부호 등)/hwp_goto.py
@@ -1,8 +1,12 @@
 import win32com.client as win32
 hwp = win32.gencache.EnsureDispatch("HWPFrame.HwpObject")
+hwp.XHwpWindows.Item(0).Visible = True
 
-hwp.HAction.GetDefault("Goto", hwp.HParameterSet.HGotoE.HSet)
-hwp.HParameterSet.HGotoE.HSet.SetItem("DialogResult", 2) # 2쪽
-hwp.HParameterSet.HGotoE.SetSelectionIndex = 1 # "쪽" 찾아가기
-hwp.HAction.Execute("Goto", hwp.HParameterSet.HGotoE.HSet)
-hwp.Run("Cancel")
+
+def goto_page(page):
+    hwp.HAction.GetDefault("Goto", hwp.HParameterSet.HGotoE.HSet)
+    hwp.HParameterSet.HGotoE.SetSelectionIndex = 1
+    hwp.HParameterSet.HGotoE.HSet.SetItem("DialogResult", page)
+    hwp.HAction.Execute("Goto", hwp.HParameterSet.HGotoE.HSet)
+
+goto_page(3)
